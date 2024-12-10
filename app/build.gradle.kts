44a7aa13384942c9e749bb3af1ddee8a7a965d0b
daviloz <davidloz94.dls@gmail.com>
2024-12-10 20:36:19 +0100

    Feature: Toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "edu.iesam.ddi_tarea_6_ui"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "edu.iesam.ddi_tarea_6_ui"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
